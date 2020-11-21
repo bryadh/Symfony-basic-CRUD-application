03e1ceac41e5173d58abe8457c570c4336f7fae1
bryadh <ryadh.belghanem@gmail.com>
2020-11-21 23:04:52 +0100

    app completed

---
+++ src/Entity/Article.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ArticleRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ArticleRepository::class)
+ */
+class Article
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="text", length=100)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $body;
+
+    // GETTER AND SETTER
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    function getTitle()
+    {
+        return $this->title;
+    }
+
+    function setTitle($title)
+    {
+        $this->title = $title;
+    }
+
+    function getBody()
+    {
+        return $this->body;
+    }
+
+    function setBody($body)
+    {
+        $this->body = $body;
+    }
+
+
+
+
+}
